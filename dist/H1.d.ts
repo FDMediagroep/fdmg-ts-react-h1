74c9ec22b7d86037467417cbdf288c746c03d18c
Willem Liu <willem_liu@hotmail.com>
2017-11-16 08:54:49 +0100

    v1.0.2 Added types

---
+++ dist/H1.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="react" />
+import * as React from 'react';
+/**
+ * H1 compononent renders a heading
+ */
+export default class H1 extends React.Component {
+    state: any;
+    props: any;
+    constructor(props: any);
+    componentDidMount(): void;
+    componentWillUnmount(): void;
+    componentWillReceiveProps(nextProps: any): void;
+    render(): JSX.Element;
+}
--- dist/H1.js
+++ dist/H1.js
@@ -32,7 +32,7 @@ var H1 = /** @class */ (function (_super) {
         console.info('H1 next props', nextProps);
     };
     H1.prototype.render = function () {
-        return (React.createElement("h1", { className: this.props.className ? this.props.className : null }, this.props.text));
+        return (React.createElement("h1", { className: this.props.className }, this.props.text));
     };
     return H1;
 }(React.Component));
